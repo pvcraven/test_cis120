4d63b4dcc64657dc8a399f33abdc4caf63f87539
Paul Craven <paul.craven@simpson.edu>
2019-09-10 14:31:33 -0500

    Initial check-in of files.

---
+++ public_html/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en">
+     <head>
+         <meta charset="utf-8"> 
+             
+         <title>Storm Cupcakes</title>
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/journal/bootstrap.min.css">
+     </head>
+     <body>
+        <?php include '../resources/navigation.php'; ?>
+        
+        <div class="container">
+            <header>
+                <h1 style="text-align:center;font-size: 36px;
+                ">Storm Cupcakes 
+                <img src="cupcake.png" alt="Cupcake">
+                </h1>
+                </header>
+            
+                <style>
+                    p {
+                        background-color: #dedede;
+                        border-style: solid;
+                        border-width: 2px;
+                        border-radius: 2px;
+                        margin: 3px;
+                        padding: 3px;
+                    }
+                </style>
+        
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Best cupcakes in Indianola!</h1>
+                <p class="lead">Stop by and order a cupcake for you and all your friends!</p>
+            </div>
+        </div>
+        <article>
+            <p>Storm Cupcakes originated in 2006 with the intent to create more jobs for college and high school students. As well as bring more sweetness to the Indianola community.</p> 
+        </article>
+        <div class="col-md-6 col-md-6">
+            <img src="redvelvetcake.png" alt="Red Velvet Cupcake">
+            <img src="confetticake.png" alt="Confetti Cupcake">
+        </div>
+            <img src="social_media.png" alt="Social Media" />
+            <?php include '../resources/footer.php'; ?>
+        </div>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        
+     </body>
+</html>
+    
+
+        
+    
+        
